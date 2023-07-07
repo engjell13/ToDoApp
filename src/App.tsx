b8580e9571dd0756e669f272f2de70a4dd0efe4b
engjell13 <engjellm2001@gmail.com>
2023-07-07 21:38:55 +0200

    creating inputs and adding style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import "./App.css";
 
 function App() {
-  return <h1>Vite React Tailwindcss</h1>;
+  return (
+    <div className="app">
+      <div className="input__container">
+        <div className="input__wrapper">
+          <input type="text" placeholder="Task" />
+          <input type="text" placeholder="Description" />
+          <input type="number" placeholder="Deadline (days)" />
+          <button className="add__task">Add Task</button>
+        </div>
+      </div>
+      <div className="todo__list"></div>
+    </div>
+  );
 }
 
 export default App;
